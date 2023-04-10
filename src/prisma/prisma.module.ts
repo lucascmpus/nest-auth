a4ddaf9bbc246134a77ed8b53bd3eec76e03cc93
lucascmpus <lucascmpus@gmail.com>
2023-04-10 16:47:46 -0300

    chore: generate user

---
+++ src/prisma/prisma.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+
+@Module({
+  providers: [PrismaService],
+  exports: [PrismaService],
+})
+// eslint-disable-next-line prettier/prettier
+export class PrismaModule { }
